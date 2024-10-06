4289ad90c9997300908930c281207d81c5110032
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2024-10-06 18:00:57 +0800

    actions tab is done (still has no functionality)

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ class $modify(EditorPause, EditorPauseLayer)
 			auto smallActionsMenu = this->getChildByID("small-actions-menu");
 			auto togglesMenu = this->getChildByID("options-menu");
 			auto settingsMenu = this->getChildByID("settings-menu");
+			auto infoMenu = this->getChildByID("info-menu");
 
 			actionsMenu->removeMeAndCleanup();
 			smallActionsMenu->removeMeAndCleanup();
@@ -28,7 +29,7 @@ class $modify(EditorPause, EditorPauseLayer)
 			newTogglesMenu->setID("options-menu"_spr);
 			newTogglesMenu->ignoreAnchorPointForPosition(false);
 			newTogglesMenu->setScaledContentSize({150.f, 300.f});
-			newTogglesMenu->setPosition({115.f, 160.f});
+			newTogglesMenu->setPosition({90.f, 160.f});
 
 			CCScale9Sprite *TogglesMenuSprite = CCScale9Sprite::create("square02b_001.png");
 			TogglesMenuSprite->ignoreAnchorPointForPosition(false);
@@ -51,6 +52,16 @@ class $modify(EditorPause, EditorPauseLayer)
 			ActionMenuSprite->setOpacity(150);
 			ActionMenuSprite->setColor(ccColor3B(0, 0, 0));
 
+			// infoMenu background
+			CCScale9Sprite *infoMenuSprite = CCScale9Sprite::create("square02b_001.png");
+			infoMenuSprite->ignoreAnchorPointForPosition(false);
+			infoMenuSprite->setScaledContentSize(infoMenu->getScaledContentSize());
+			infoMenuSprite->setAnchorPoint({0.0, 0.0});
+			infoMenuSprite->setOpacity(150);
+			infoMenuSprite->setColor(ccColor3B(0, 0, 0));
+
+
+
 			// ACTION TAB //////////////////////////////////////////////////////////////////////////////////////
 
 			// Action Title
@@ -62,14 +73,14 @@ class $modify(EditorPause, EditorPauseLayer)
 			// Quick Action Title
 			auto QuickActionTitle = CCLabelBMFont::create("Quick Actions", "goldFont.fnt");
 			QuickActionTitle->setScale(0.65);
-			QuickActionTitle->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 175.f});
+			QuickActionTitle->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 155.f});
 			QuickActionTitle->ignoreAnchorPointForPosition(false);
 
 			// Action Buttons //////////////////////////////////////////////////////////////////////////////////////
 
 			// Build Helper Button
 			auto BuildHelper_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			BuildHelper_ButtonSprite->setScale(0.9);
+			BuildHelper_ButtonSprite->setScale(0.8);
 
 			auto BuildHelper_Sprite = CCSprite::create("a_buildHelper.png"_spr);
 			auto BuildHelper = CCMenuItemSpriteExtra::create(
@@ -87,7 +98,7 @@ class $modify(EditorPause, EditorPauseLayer)
 
 			// Copy Color Plus Button
 			auto CopyColorPlus_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			CopyColorPlus_ButtonSprite->setScale(0.9);
+			CopyColorPlus_ButtonSprite->setScale(0.8);
 
 			auto CopyColorPlus_Sprite = CCSprite::create("a_copyColor+.png"_spr);
 			auto CopyColorPlus = CCMenuItemSpriteExtra::create(
@@ -105,7 +116,7 @@ class $modify(EditorPause, EditorPauseLayer)
 
 			// Paste Color Plus Button
 			auto PasteColorPlus_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			PasteColorPlus_ButtonSprite->setScale(0.9);
+			PasteColorPlus_ButtonSprite->setScale(0.8);
 
 			auto PasteColorPlus_Sprite = CCSprite::create("a_pasteColor+.png"_spr);
 			auto PasteColor = CCMenuItemSpriteExtra::create(
@@ -123,7 +134,7 @@ class $modify(EditorPause, EditorPauseLayer)
 
 			// Create Extra Button
 			auto CreateExtra_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			CreateExtra_ButtonSprite->setScale(0.9);
+			CreateExtra_ButtonSprite->setScale(0.8);
 
 			auto CreateExtra_Sprite = CCSprite::create("a_createExtras.png"_spr);
 			auto CreateExtra = CCMenuItemSpriteExtra::create(
@@ -137,11 +148,11 @@ class $modify(EditorPause, EditorPauseLayer)
 			CreateExtra_Sprite->setScale(0.8);
 
 			CreateExtra->setZOrder(1);
-			CreateExtra->setPosition({newActionsMenu->getContentWidth() / 2.f - 50.f, newActionsMenu->getContentHeight() - 95.f});
+			CreateExtra->setPosition({newActionsMenu->getContentWidth() / 2.f - 50.f, newActionsMenu->getContentHeight() - 90.f});
 
 			// Unlock Layers Button
 			auto UnlockLayers_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			UnlockLayers_ButtonSprite->setScale(0.9);
+			UnlockLayers_ButtonSprite->setScale(0.8);
 
 			auto UnlockLayers_Sprite = CCSprite::create("a_unlockLayers.png"_spr);
 			auto UnlockLayers = CCMenuItemSpriteExtra::create(
@@ -155,11 +166,11 @@ class $modify(EditorPause, EditorPauseLayer)
 			UnlockLayers_Sprite->setScale(0.8);
 
 			UnlockLayers->setZOrder(1);
-			UnlockLayers->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 95.f});
+			UnlockLayers->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 90.f});
 
 			// Reset Unused Button
 			auto ResetUnused_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			ResetUnused_ButtonSprite->setScale(0.9);
+			ResetUnused_ButtonSprite->setScale(0.8);
 
 			auto ResetUnused_Sprite = CCSprite::create("a_resetUnused.png"_spr);
 			auto ResetUnused = CCMenuItemSpriteExtra::create(
@@ -173,11 +184,30 @@ class $modify(EditorPause, EditorPauseLayer)
 			ResetUnused_Sprite->setScale(0.8);
 
 			ResetUnused->setZOrder(1);
-			ResetUnused->setPosition({newActionsMenu->getContentWidth() / 2.f + 50.f, newActionsMenu->getContentHeight() - 95.f});
+			ResetUnused->setPosition({newActionsMenu->getContentWidth() / 2.f + 50.f, newActionsMenu->getContentHeight() - 90.f});
+
+			// Create Loop Button
+			auto CreateLoop_ButtonSprite = CCSprite::create("GJ_button_04.png");
+			CreateLoop_ButtonSprite->setScale(0.8);
+
+			auto CreateLoop_Sprite = CCSprite::create("a_createLoop.png"_spr);
+			auto CreateLoop = CCMenuItemSpriteExtra::create(
+				CreateLoop_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			CreateLoop_ButtonSprite->addChild(CreateLoop_Sprite);
+
+			CreateLoop_Sprite->setAnchorPoint({0.0, 0.0});
+			CreateLoop_Sprite->setPosition({4.0, 4.0});
+			CreateLoop_Sprite->setScale(0.65);
+
+			CreateLoop->setZOrder(1);
+			CreateLoop->setPosition({newActionsMenu->getContentWidth() / 2.f - 50.f, newActionsMenu->getContentHeight() - 130.f});
 
 			// Uncheck Portals Button
 			auto UncheckPortals_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			UncheckPortals_ButtonSprite->setScale(0.9);
+			UncheckPortals_ButtonSprite->setScale(0.8);
 
 			auto UncheckPortals_Sprite = CCSprite::create("a_uncheckPortals.png"_spr);
 			auto UncheckPortals = CCMenuItemSpriteExtra::create(
@@ -191,11 +221,11 @@ class $modify(EditorPause, EditorPauseLayer)
 			UncheckPortals_Sprite->setScale(0.8);
 
 			UncheckPortals->setZOrder(1);
-			UncheckPortals->setPosition({newActionsMenu->getContentWidth() / 2.f - 25.f, newActionsMenu->getContentHeight() - 145.f});
+			UncheckPortals->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 130.f});
 
 			// Keys Button
 			auto Keys_ButtonSprite = CCSprite::create("GJ_button_04.png");
-			Keys_ButtonSprite->setScale(0.9);
+			Keys_ButtonSprite->setScale(0.8);
 
 			auto Keys_Sprite = CCSprite::create("a_keys.png"_spr);
 			auto Keys = CCMenuItemSpriteExtra::create(
@@ -209,9 +239,189 @@ class $modify(EditorPause, EditorPauseLayer)
 			Keys_Sprite->setScale(0.8);
 
 			Keys->setZOrder(1);
-			Keys->setPosition({newActionsMenu->getContentWidth() / 2.f + 25.f, newActionsMenu->getContentHeight() - 145.f});
+			Keys->setPosition({newActionsMenu->getContentWidth() / 2.f + 50.f, newActionsMenu->getContentHeight() - 130.f});
 
 			// Quick Actions //////////////////////////////////////////////////////////////////////////////////////
+
+			// Regroup Button
+			auto Regroup_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			Regroup_ButtonSprite->setScale(0.8);
+
+			auto Regroup_Sprite = CCSprite::create("as_regroup.png"_spr);
+			auto Regroup = CCMenuItemSpriteExtra::create(
+				Regroup_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			Regroup_ButtonSprite->addChild(Regroup_Sprite);
+
+			Regroup_Sprite->setAnchorPoint({0.0, 0.0});
+			Regroup_Sprite->setPosition({4.0, 4.0});
+			Regroup_Sprite->setScale(0.65);
+
+			Regroup->setZOrder(1);
+			Regroup->setPosition({newActionsMenu->getContentWidth() / 2.f - 50.f, newActionsMenu->getContentHeight() - 185.f});
+
+			// Reset Scroll Button
+			auto ResetScroll_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			ResetScroll_ButtonSprite->setScale(0.8);
+
+			auto ResetScroll_Sprite = CCSprite::create("as_resetScroll.png"_spr);
+			auto ResetScroll = CCMenuItemSpriteExtra::create(
+				ResetScroll_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			ResetScroll_ButtonSprite->addChild(ResetScroll_Sprite);
+
+			ResetScroll_Sprite->setAnchorPoint({0.0, 0.0});
+			ResetScroll_Sprite->setPosition({4.0, 4.0});
+			ResetScroll_Sprite->setScale(0.65);
+
+			ResetScroll->setZOrder(1);
+			ResetScroll->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 185.f});
+
+			// Select All Button
+			auto SelectAll_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			SelectAll_ButtonSprite->setScale(0.8);
+
+			auto SelectAll_Sprite = CCSprite::create("as_selectAll.png"_spr);
+			auto SelectAll = CCMenuItemSpriteExtra::create(
+				SelectAll_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			SelectAll_ButtonSprite->addChild(SelectAll_Sprite);
+
+			SelectAll_Sprite->setAnchorPoint({0.0, 0.0});
+			SelectAll_Sprite->setPosition({4.0, 4.0});
+			SelectAll_Sprite->setScale(0.65);
+
+			SelectAll->setZOrder(1);
+			SelectAll->setPosition({newActionsMenu->getContentWidth() / 2.f + 50.f, newActionsMenu->getContentHeight() - 185.f});
+
+			// Align X Button
+			auto AlignX_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			AlignX_ButtonSprite->setScale(0.8);
+
+			auto AlignX_Sprite = CCSprite::create("as_alignX.png"_spr);
+			auto AlignX = CCMenuItemSpriteExtra::create(
+				AlignX_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			AlignX_ButtonSprite->addChild(AlignX_Sprite);
+
+			AlignX_Sprite->setAnchorPoint({0.0, 0.0});
+			AlignX_Sprite->setPosition({4.0, 4.0});
+			AlignX_Sprite->setScale(0.65);
+
+			AlignX->setZOrder(1);
+			AlignX->setPosition({newActionsMenu->getContentWidth() / 2.f - 50.f, newActionsMenu->getContentHeight() - 225.f});
+
+			// New Group X Button
+			auto NewGroupX_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			NewGroupX_ButtonSprite->setScale(0.8);
+
+			auto NewGroupX_Sprite = CCSprite::create("as_newGroupX.png"_spr);
+			auto NewGroupX = CCMenuItemSpriteExtra::create(
+				NewGroupX_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			NewGroupX_ButtonSprite->addChild(NewGroupX_Sprite);
+
+			NewGroupX_Sprite->setAnchorPoint({0.0, 0.0});
+			NewGroupX_Sprite->setPosition({4.0, 4.0});
+			NewGroupX_Sprite->setScale(0.65);
+
+			NewGroupX->setZOrder(1);
+			NewGroupX->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 225.f});
+
+			// Select All Left Button
+			auto SelectAllLeft_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			SelectAllLeft_ButtonSprite->setScale(0.8);
+
+			auto SelectAllLeft_Sprite = CCSprite::create("as_selectAllLeft.png"_spr);
+			auto SelectAllLeft = CCMenuItemSpriteExtra::create(
+				SelectAllLeft_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			SelectAllLeft_ButtonSprite->addChild(SelectAllLeft_Sprite);
+
+			SelectAllLeft_Sprite->setAnchorPoint({0.0, 0.0});
+			SelectAllLeft_Sprite->setPosition({4.0, 4.0});
+			SelectAllLeft_Sprite->setScale(0.65);
+
+			SelectAllLeft->setZOrder(1);
+			SelectAllLeft->setPosition({newActionsMenu->getContentWidth() / 2.f + 50.f, newActionsMenu->getContentHeight() - 225.f});
+
+			// Align Y Button
+			auto AlignY_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			AlignY_ButtonSprite->setScale(0.8);
+
+			auto AlignY_Sprite = CCSprite::create("as_alignY.png"_spr);
+			auto AlignY = CCMenuItemSpriteExtra::create(
+				AlignY_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			AlignY_ButtonSprite->addChild(AlignY_Sprite);
+
+			AlignY_Sprite->setAnchorPoint({0.0, 0.0});
+			AlignY_Sprite->setPosition({4.0, 4.0});
+			AlignY_Sprite->setScale(0.65);
+
+			AlignY->setZOrder(1);
+			AlignY->setPosition({newActionsMenu->getContentWidth() / 2.f - 50.f, newActionsMenu->getContentHeight() - 265.f});
+
+			// New Group Y Button
+			auto NewGroupY_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			NewGroupY_ButtonSprite->setScale(0.8);
+
+			auto NewGroupY_Sprite = CCSprite::create("as_newGroupY.png"_spr);
+			auto NewGroupY = CCMenuItemSpriteExtra::create(
+				NewGroupY_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			NewGroupY_ButtonSprite->addChild(NewGroupY_Sprite);
+
+			NewGroupY_Sprite->setAnchorPoint({0.0, 0.0});
+			NewGroupY_Sprite->setPosition({4.0, 4.0});
+			NewGroupY_Sprite->setScale(0.65);
+
+			NewGroupY->setZOrder(1);
+			NewGroupY->setPosition({newActionsMenu->getContentWidth() / 2.f, newActionsMenu->getContentHeight() - 265.f});
+
+			// Select All Right Button
+			auto SelectAllRight_ButtonSprite = CCSprite::create("GJ_button_05.png");
+			SelectAllRight_ButtonSprite->setScale(0.8);
+
+			auto SelectAllRight_Sprite = CCSprite::create("as_selectAllRight.png"_spr);
+			auto SelectAllRight = CCMenuItemSpriteExtra::create(
+				SelectAllRight_ButtonSprite,
+				this,
+				menu_selector(EditorPause::onNewToggles)); // Change this to function
+			
+			SelectAllRight_ButtonSprite->addChild(SelectAllRight_Sprite);
+
+			SelectAllRight_Sprite->setAnchorPoint({0.0, 0.0});
+			SelectAllRight_Sprite->setPosition({4.0, 4.0});
+			SelectAllRight_Sprite->setScale(0.65);
+
+			SelectAllRight->setZOrder(1);
+			SelectAllRight->setPosition({newActionsMenu->getContentWidth() / 2.f + 50.f, newActionsMenu->getContentHeight() - 265.f});
+
+
+			// TOGGLE TAB //////////////////////////////////////////////////////////////////////////////////////
+
+			// Options Title
+			auto OptionTitle = CCLabelBMFont::create("Options", "goldFont.fnt");
+			OptionTitle->setScale(0.8);
+			OptionTitle->setPosition({newTogglesMenu->getContentWidth() / 2.f, newTogglesMenu->getContentHeight() - 15.f});
+			OptionTitle->ignoreAnchorPointForPosition(false);
 
 			// Add Button to Action menu
 			newActionsMenu->addChild(BuildHelper);
@@ -220,8 +430,20 @@ class $modify(EditorPause, EditorPauseLayer)
 			newActionsMenu->addChild(CreateExtra);
 			newActionsMenu->addChild(UnlockLayers);
 			newActionsMenu->addChild(ResetUnused);
+			newActionsMenu->addChild(CreateLoop);
 			newActionsMenu->addChild(UncheckPortals);
 			newActionsMenu->addChild(Keys);
+
+			newActionsMenu->addChild(Regroup);
+			newActionsMenu->addChild(ResetScroll);
+			newActionsMenu->addChild(SelectAll);
+			newActionsMenu->addChild(AlignX);
+			newActionsMenu->addChild(NewGroupX);
+			newActionsMenu->addChild(SelectAllLeft);
+			newActionsMenu->addChild(AlignY);
+			newActionsMenu->addChild(NewGroupY);
+			newActionsMenu->addChild(SelectAllRight);
+
 
 			// Add Menu into the Editor Pause Layer
 			this->addChild(newActionsMenu);
@@ -229,10 +451,14 @@ class $modify(EditorPause, EditorPauseLayer)
 
 			newActionsMenu->addChild(ActionMenuSprite);
 			newTogglesMenu->addChild(TogglesMenuSprite);
+			infoMenu->addChild(infoMenuSprite);
 			
 			// Add Title to the Action Menu
 			newActionsMenu->addChild(ActionTitle);
 			newActionsMenu->addChild(QuickActionTitle);
+
+			// Add Title to the Toggles Menu
+			newTogglesMenu->addChild(OptionTitle);
 
 			return true;
 		}
